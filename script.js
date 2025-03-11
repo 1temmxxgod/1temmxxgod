dfa2a605fd2f2c58313a81815288f05be64d7921
1temmxxgod <152515503+1temmxxgod@users.noreply.github.com>
2025-03-11 20:18:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('participate-btn').addEventListener('click', function() {
+    // Здесь можно добавить логику для участия, например, перенаправление в Telegram
+    alert('Нажмите "ОК", чтобы перейти в Telegram для участия!');
+    window.location.href = 'https://t.me/Overhaul_Giveaway_Bot'; // Замените на ссылку на ваш Telegram-бот
+});
